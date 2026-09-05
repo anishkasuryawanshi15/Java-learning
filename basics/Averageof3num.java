0d79f51f00a6ea78183191e0808d14be32e174dc
anishkasuryawanshi15 <anishka2025@gmail.com>
2026-09-05 12:34:50 +0530

    Add average of 3 numbers

---
+++ basics/Averageof3num.java
@@ -0,0 +1,13 @@
+package basics;
+import java.util.*;
+public class Averageof3num {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int average = (a + b + c) / 3;
+        System.out.println("Average of 3 numbers is: " + average); 
+        sc.close();
+    }
+}
