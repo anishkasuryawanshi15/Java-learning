980c57dc3159077d280c47d43e48142174d25dc7
anishkasuryawanshi15 <anishka2025@gmail.com>
2026-09-07 23:06:23 +0530

    Add variables basics

---
+++ basics/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Declaring and assigning variables
+        int age = 20;
+        double height = 5.8;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Anishka";
+
+        // Printing the variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
