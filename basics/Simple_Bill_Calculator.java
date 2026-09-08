5930f8dc52c07e2f1ecaef420f5b0eefab4a37fa
anishkasuryawanshi15 <anishka2025@gmail.com>
2026-09-08 22:21:45 +0530

    Add item bill Calculator

---
+++ basics/Simple_Bill_Calculator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Simple_Bill_Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float pencil = sc.nextFloat();
+        float pen = sc.nextFloat();
+        float eraser = sc.nextFloat();
+
+        float total = pencil + pen + eraser;
+        System.out.println("Total: " + total);
+
+        //18% tax
+        float tax = total + (0.18f * total);
+
+        System.out.println("Bill after 18% tax: " + tax);
+    }
+}
